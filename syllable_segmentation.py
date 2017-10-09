785e85d81a6ea57b4356d514c2156b018c260236
Mike <mikeabyrvalg5@gmail.com>
2017-10-09 23:51:38 +0300

    syllable segmentation final version + testing

--- syllable_segmentation.py
+++ syllable_segmentation.py
@@ -1,36 +1,12 @@
-"""# coding=utf-8
-
-vowels = set(u'аеёиоуыэюя')
-sign_chars = set(u'ъь')
-pattern_str = u"(c*[ьъ]?vc+[ьъ](?=v))|(c*[ьъ]?v(?=v|cv))|(c*[ьъ]?vc[ъь]?(?=cv|ccv))|(c*[ьъ]?v[cьъ]*(?=$))"
-pattern = re.compile(pattern_str)
-tokenizing = re.compile('\w+')
-
-
-def get_syllables(word):
-    mask = ''.join(['v' if c in vowels else c if c in sign_chars else 'c' for c in word.lower()])
-    return [word[m.start():m.end()] for m in pattern.finditer(mask)]
-
-string = 'капремонт'
-
-for word in tokenizing.findall(string.lower()):
-
-    if len(word) != len(''.join(get_syllables(word))):
-        print(word)
-        print(print('-'.join(get_syllables(word))))"""
-
 import re
-import morfessor
 
-io = morfessor.MorfessorIO()
+# prepositions were supposed to be used for improving the segmentation results, but currently they are not used
 prep_finder = re.compile('\w+')
 prepositions = """без-/бес- · в-/во- · вз-/взо-/вс- · вне- · внутри- · воз-/возо-/вос- · все- · вы- · до- · за- · из-/изо-/ис- · испод- · к- · кое-/кой- · меж-/междо-/между- · на- · над-/надо- · наи- · не- · небез-/небес- · недо- · ни- · низ-/низо-/нис- · о- · об-/обо- · обез-/обес- · около- · от-/ото- · па- · пере- · по- · под-/подo- · поза- · после- · пра- · пре- · пред-/предо- · преди- · при- · про- · противо- · раз-/разо-/рас- (роз-/рос-) · с-/со- · сверх- · среди- · су- · сыз- · тре- · у- · чрез-/через-/черес-
 
 а- · анти- · архи- · би- · вице- · гипер- · де-/дез- · дис- · им- · интер- · ир- · квази- · контр- · макро- · микро- · обер- · пост- · пре- · прото- · псевдо- · ре- · суб- · супер- · транс- · ультра- · экзо- · экс- · экстра-
 
 агит- · глав- · гор- · гос- · деп- · дет- · диа- · здрав- · ино- · кол- · ком- · лик- · маг- · мат- · маш- · мин- · мол- · об-/обл- · окруж- · орг- · парт- · полит- · потреб- · прод- · пром- · проп- · рай- · рег- · ред- · род- · рос- · сек- · сель- · со- · сов- · сот- · соц- · студ- · тер- · фед- · фин- · хоз- · хос-"""
-
-# print(prep_finder.findall(prepositions))
 
 vowel = set('аеёиоуыэюя')
 voiced = set('бвгдзлмнрхц')
@@ -77,30 +53,39 @@ def get_syllables(string):
 
         # если эта буква - не последняя и не предпоследняя, текущая - гласная, следующая - глухая согласная,
         # а после неё - согласная, и это - не последний слог
-        if ind < last_ind - 1 and letter in vowel and string[ind+1] in deaf and string[ind+2] in cons and check_vowel_presence(string[ind+1:]):
+        if ind < last_ind - 1 and letter in vowel and string[ind+1] in deaf and string[ind+2] in cons and \
+                check_vowel_presence(string[ind+1:]):
             result.append(current_syllable)
             current_syllable = ''
 
         # если это - не первая и не последняя буква, если текущая - звонкая или шипящая согласная, перед ней - гласная,
         # следующая - не гласная и не "ь"/"ъ", и это - не последний слог
-        if ind != 0 and ind != last_ind and letter in voiced.union(sizz) and string[ind-1] in vowel and string[ind+1] not in vowel.union(other) and check_vowel_presence(string[ind+1:]):
+        if ind != 0 and ind != last_ind and letter in voiced.union(sizz) and string[ind-1] in vowel and \
+                string[ind+1] not in vowel.union(other) and check_vowel_presence(string[ind+1:]):
             result.append(current_syllable)
             current_syllable = ''
 
         # если текущая - "ь"/"ъ" и при этом не последняя, если следующая - гласная или первый слог
-        if ind != last_ind and letter in other and (string[ind+1] not in vowel and result == []):
-            result.append(current_syllable)
-            current_syllable = ''
-
+        # или если следуют от 1 до 3 согласных, за которыми - гласная
+        # ъ/ь не должны быть второй буквой (въехать), перед ъ/ь не должно быть двух согласных подряд
+        try:        
+            if ind != last_ind and ind != 1 and string[ind-2] in vowel and letter in other and ((string[ind+1] in vowel) or result == [] or (string[ind+1] in cons and string[ind+2] in vowel) or (string[ind+1] in cons and string[ind+2] in cons and string[ind+3] in vowel) or (string[ind+1] in cons and string[ind+2] in cons and string[ind+3] in cons and string[ind+4] in vowel)):
+                result.append(current_syllable)
+                current_syllable = ''
+        except IndexError:
+            pass
+        
     result.append(current_syllable)
 
     return '-'.join(result)
 
-with open('сложные-слова.txt', 'r', encoding='utf-8') as opener:
-    word_list = opener.read().split('\n')
 
-model_types = io.read_binary_model_file(r'C:\Users\Ольга\PycharmProjects\DSM_morphology\morfessor\types')
+if __name__ == "__main__":
 
-for word in word_list:
-    syllabs = get_syllables(word.lower())
-    print(syllabs)
+    with open('ьъ.txt', 'r', encoding='utf-8') as opener:
+        word_list = opener.read().split('\n')
+
+    for word in word_list:
+        print(word)
+        syllabs = get_syllables(word.lower())
+        print(syllabs)
