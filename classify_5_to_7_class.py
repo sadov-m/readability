601184e2efa9dbc9c7ab833cdfb5b3e3294d9cf4
Mike <mikeabyrvalg5@gmail.com>
2018-10-09 20:44:10 +0300

    final commit

--- classify_5_to_7_class.py
+++ classify_5_to_7_class.py
@@ -5,6 +5,7 @@ import numpy as np
 path_for_pipeline = input('type in the path to a folder with texts to analyze: ')
 
 paths = extracting_texts_paths(path_for_pipeline)
+#x = []
 
 
 def classify_5_to_7(paths_list):
@@ -15,18 +16,23 @@ def classify_5_to_7(paths_list):
 
             metrics = IB_metrics_readability.calc_readability_metrics(text)
             avg = np.mean([metrics['FK'], metrics['CL'], metrics['DC'], metrics['SMOG'], metrics['ARI']])
+            #x.append(avg)
 
-            if avg < 2:
+            if avg < 3:
                 results.append(4)  # too easy for such pupils to read
-            elif 2 <= avg < 4:
+            elif 3 <= avg < 8.5:
                 results.append(5)
-            elif 4 <= avg < 7:
+            elif 8.5 <= avg < 11:
                 results.append(6)
-            elif 7 <= avg < 9:
+            elif 11 <= avg < 13.5:
                 results.append(7)
             else:
                 results.append(8)  # too hard for such pupils to read
     return results
 
 
-#print(classify_5_to_7(paths))
+"""import matplotlib.pyplot as plt
+
+num_bins = 10
+n, bins, patches = plt.hist(x, num_bins, facecolor='blue', alpha=0.5)
+plt.show()"""
